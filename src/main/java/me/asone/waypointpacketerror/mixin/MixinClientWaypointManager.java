dda3721e76eeba97deca974c84d763980f4bb60b
AsOne2020 <59190831+AsOne2020@users.noreply.github.com>
2026-01-17 20:40:35 +0800

    fix(mixin): 修复客户端路径点管理器空指针异常

--- src/main/java/me/asone/waypointpacketerror/mixin/MixinClientWaypointManager.java
+++ src/main/java/me/asone/waypointpacketerror/mixin/MixinClientWaypointManager.java
@@ -1,4 +1,32 @@
 package me.asone.waypointpacketerror.mixin;
 
+import com.mojang.datafixers.util.Either;
+import net.minecraft.client.waypoints.ClientWaypointManager;
+import net.minecraft.world.waypoints.TrackedWaypoint;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Map;
+import java.util.UUID;
+
+@Mixin(ClientWaypointManager.class)
 public class MixinClientWaypointManager {
+
+	@Shadow @Final private Map<Either<UUID, String>, TrackedWaypoint> waypoints;
+
+	@Inject(
+			method = "updateWaypoint(Lnet/minecraft/world/waypoints/TrackedWaypoint;)V",
+			at = @At("HEAD"),
+			cancellable = true
+	)
+	private void guardNullWaypoint(TrackedWaypoint trackedWaypoint, CallbackInfo ci) {
+		if (this.waypoints.get(trackedWaypoint.id()) == null) {
+			ci.cancel();
+		}
+	}
+
 }
